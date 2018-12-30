78fd4817b2b56b28a9bb3c3810be2179f9f1ac46
Alexandr Nikitenko <sashanikitenko74@gmail.com>
2018-12-30 20:44:22 +0200

    Update MetroCard.py

--- AtTheCrossroads/MetroCard.py
+++ AtTheCrossroads/MetroCard.py
@@ -21,7 +21,6 @@ you extend your card.
 """
 
 def metroCard(lastNumberOfDays):
-    if lastNumberOfDays == 28: return [31]
-    if lastNumberOfDays == 30: return [31]
+    if lastNumberOfDays == 28 or lastNumberOfDays == 30: return [31]
     if lastNumberOfDays == 31: return [28, 30, 31]
     return []
